e82bb430f5902906435183aaa879ff1cdc36ed7d
Jun Xue <81678987+JunXue-tech@users.noreply.github.com>
2024-09-05 17:59:52 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,240 +1,145 @@
-from __future__ import print_function
-import os
-import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
-from torch.autograd import Variable
 from torch.nn import Parameter
+from torch.autograd import Variable
 
-
-class Local_Attention(nn.Module):
+class LocalAttention(nn.Module):
     def __init__(self, k_size=3):
-        super(Local_Attention, self).__init__()
+        super(LocalAttention, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.conv1 = nn.Conv1d(1, 1, kernel_size=3,
-                               padding=(k_size - 1) // 2, bias=False)
+        self.conv1 = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
         self.sigmoid = nn.Sigmoid()
+
     def forward(self, x):
-        y = self.avg_pool(x)
-        y = y.squeeze(-1)
-        y = y.transpose(-1, -2)
-        y = self.conv1(y)
-        y = y.transpose(-1, -2)
-        y = y.unsqueeze(-1)
+        y = self.avg_pool(x).squeeze(-1).transpose(-1, -2)
+        y = self.conv1(y).transpose(-1, -2).unsqueeze(-1)
         y = self.sigmoid(y)
         return x * y.expand_as(x)
 
-class Spatial_Reconstruction(nn.Module):
-    def __init__(self, ):
-        super(Spatial_Reconstruction, self).__init__()
-        self.conv1 = DEPTHWISECONV2d(dim=1)
-        self.drop = nn.Dropout(p=0.5)
+class SpatialReconstruction(nn.Module):
+    def __init__(self):
+        super(SpatialReconstruction, self).__init__()
+        self.depthwise_conv = DepthwiseConv2d(dim=1)
+        self.dropout = nn.Dropout(p=0.5)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        y = x
         avg_out = torch.mean(x, dim=1, keepdim=True)
-        x = self.conv1(avg_out)
-        x=self.drop(x)
-        return self.sigmoid(x) * y
+        x = self.depthwise_conv(avg_out)
+        x = self.dropout(x)
+        return self.sigmoid(x) * x
 
-class DEPTHWISECONV2d(nn.Module):
+class DepthwiseConv2d(nn.Module):
     def __init__(self, dim):
-        super(DEPTHWISECONV2d, self).__init__()
+        super(DepthwiseConv2d, self).__init__()
         self.depth_conv = nn.Conv2d(dim, dim, kernel_size=7, stride=1, padding=6, groups=dim, dilation=2)
-        # self.point_conv = nn.Conv2d(dim, dim, kernel_size=1, stride=1, padding=0, groups=1)
 
     def forward(self, input):
-        out = self.depth_conv(input)
-        # out = self.point_conv(out)
-        return out
+        return self.depth_conv(input)
 
-
-class SR_LA_block(nn.Module):
+class SRLABlock(nn.Module):
     expansion = 2
-    def __init__(self,
-                 inplanes,
-                 planes,
-                 stride=1,
-                 downsample=None,
-                 baseWidth=26,
-                 scale=8,
-                 stype='normal'):
-        super(SR_LA_block, self).__init__()
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, baseWidth=26, scale=8, stype='normal'):
+        super(SRLABlock, self).__init__()
         width = int(math.floor(planes * (baseWidth / 64.0)))
-        self.conv1 = nn.Conv2d(inplanes,
-                               width * scale,
-                               kernel_size=1,
-                               bias=False)
+        self.conv1 = nn.Conv2d(inplanes, width * scale, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(width * scale)
-        self.SR = Spatial_Reconstruction()
-        if scale == 1:
-            self.nums = 1
-        else:
-            self.nums = scale - 1
+        self.SR = SpatialReconstruction()
+        self.nums = scale - 1 if scale > 1 else 1
+        self.stype = stype
+
         if stype == 'stage':
             self.pool = nn.AvgPool2d(kernel_size=3, stride=stride, padding=1)
-        convs = []
-        bns = []
-        for i in range(self.nums):
 
-            convs.append(
-                nn.Conv2d(width,
-                          width,
-                          kernel_size=3,
-                          stride=stride,
-                          padding=1,
-                          bias=False))
-            bns.append(nn.BatchNorm2d(width))
-        self.convs = nn.ModuleList(convs)
-        self.bns = nn.ModuleList(bns)
+        self.convs = nn.ModuleList([nn.Conv2d(width, width, kernel_size=3, stride=stride, padding=1, bias=False) for _ in range(self.nums)])
+        self.bns = nn.ModuleList([nn.BatchNorm2d(width) for _ in range(self.nums)])
 
-        self.conv3 = nn.Conv2d(width * scale,
-                               planes * self.expansion,
-                               kernel_size=1,
-                               bias=False)
+        self.conv3 = nn.Conv2d(width * scale, planes * self.expansion, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes * self.expansion)
-        self.LA = Local_Attention(planes)
+        self.LA = LocalAttention(planes)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
-        self.stype = stype
-        self.scale = scale
-        self.width = width
 
     def forward(self, x):
         residual = x
-        # print('x: ', x.size())
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        spx = torch.split(out, self.width, 1)
+        out = self.relu(self.bn1(self.conv1(x)))
+        spx = torch.split(out, self.convs[0].in_channels, 1)
+
         for i in range(self.nums):
-            if i == 0 or self.stype == 'stage':
-                sp = spx[i]
-            else:
-                at = spx[i]
-                at = self.SR(at)
-                sp = sp + at
-            sp = self.convs[i](sp)
-            sp = self.relu(self.bns[i](sp))
-            if i == 0:
-                out = sp
-            else:
-                out = torch.cat((out, sp), 1)
-        if self.scale != 1 and self.stype == 'normal':
-            out = torch.cat((out, spx[self.nums]), 1)
-        elif self.scale != 1 and self.stype == 'stage':
-            out = torch.cat((out, self.pool(spx[self.nums])), 1)
-        out = self.conv3(out)
-        # print('conv3: ', out.size())
-        out = self.bn3(out)
-        out = self.LA(out)
-        if self.downsample is not None:
+            sp = spx[i] if i == 0 or self.stype == 'stage' else sp + self.SR(spx[i])
+            sp = self.relu(self.bns[i](self.convs[i](sp)))
+            out = torch.cat([out, sp], 1) if i != 0 else sp
+
+        if self.scale != 1:
+            out = torch.cat([out, spx[self.nums] if self.stype == 'normal' else self.pool(spx[self.nums])], 1)
+
+        out = self.LA(self.bn3(self.conv3(out)))
+        if self.downsample:
             residual = self.downsample(x)
-        out += residual
-        out = self.relu(out)
-        return out
+        return self.relu(out + residual)
 
-
-
-
-class SR_LA_Res2Net(nn.Module):
-    def __init__(self, block, layers, baseWidth=26, scale=8, m=0.35, num_classes=2, loss='softmax', **kwargs):
-        self.inplanes = 16
-        super(SR_LA_Res2Net, self).__init__()
+class SRLARes2Net(nn.Module):
+    def __init__(self, block, layers, baseWidth=26, scale=8, num_classes=2, loss='softmax'):
+        super(SRLARes2Net, self).__init__()
         self.loss = loss
-        self.baseWidth = baseWidth
-        self.scale = scale
-        self.conv1 = nn.Sequential(nn.Conv2d(1, 16, 3, 1, 1, bias=False),
-                                   nn.BatchNorm2d(16), nn.ReLU(inplace=True),
-                                   nn.Conv2d(16, 16, 3, 1, 1, bias=False),
-                                   nn.BatchNorm2d(16), nn.ReLU(inplace=True),
-                                   nn.Conv2d(16, 16, 3, 1, 1, bias=False))
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 16, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(16, 16, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(16, 16, 3, 1, 1, bias=False)
+        )
         self.bn1 = nn.BatchNorm2d(16)
-        self.relu = nn.ReLU()
-        self.layer1 = self._make_layer(block, 16, layers[0])  # 64
-        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)  # 128
-        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)  # 256
-        self.layer4 = self._make_layer(block, 128, layers[3], stride=2)  # 512
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 16, layers[0])
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 128, layers[3], stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d(1)
         self.classifier = AngleLinear(128 * block.expansion, num_classes)
+
         if self.loss == 'softmax':
             self.cls_layer = nn.Sequential(nn.Linear(128 * block.expansion, num_classes), nn.LogSoftmax(dim=-1))
             self.loss_F = nn.NLLLoss()
+
+        self._init_weights()
+
+    def _init_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight,
-                                        mode='fan_out',
-                                        nonlinearity='relu')
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
+
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.AvgPool2d(kernel_size=stride,
-                             stride=stride,
-                             ceil_mode=True,
-                             count_include_pad=False),
-                nn.Conv2d(self.inplanes,
-                          planes * block.expansion,
-                          kernel_size=1,
-                          stride=1,
-                          bias=False),
+                nn.AvgPool2d(kernel_size=stride, stride=stride, ceil_mode=True, count_include_pad=False),
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=1, bias=False),
                 nn.BatchNorm2d(planes * block.expansion),
             )
-
-        layers = []
-        layers.append(
-            block(self.inplanes,
-                  planes,
-                  stride,
-                  downsample=downsample,
-                  stype='stage',
-                  baseWidth=self.baseWidth,
-                  scale=self.scale))
+        layers = [block(self.inplanes, planes, stride, downsample=downsample, stype='stage', baseWidth=self.baseWidth, scale=self.scale)]
         self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(
-                block(self.inplanes,
-                      planes,
-                      baseWidth=self.baseWidth,
-                      scale=self.scale))
-
+        layers += [block(self.inplanes, planes, baseWidth=self.baseWidth, scale=self.scale) for _ in range(1, blocks)]
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        # x = x[:, None, ...]
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
+        x = self.relu(self.bn1(self.conv1(x)))
         x = self.layer1(x)
-        # print('layer1: ', x.size())
         x = self.layer2(x)
-        # print('layer2: ', x.size())
         x = self.layer3(x)
-        # print('layer3: ', x.size())
         x = self.layer4(x)
-        # print('layer4: ', x.size())
-        # x = self.stats_pooling(x)
         x = self.avgpool(x)
-        # print('avgpool:', x.size())
-        # x = x.view(x.size(0), -1)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
-
         return x
-
-
-
-def myphi(x, m):
-    x = x * m
-    return 1 - x ** 2 / math.factorial(2) + x ** 4 / math.factorial(4) - x ** 6 / math.factorial(6) + \
-           x ** 8 / math.factorial(8) - x ** 9 / math.factorial(9)
-
 
 class AngleLinear(nn.Module):
     def __init__(self, in_features, out_features, m=4, phiflag=True):
@@ -242,7 +147,7 @@ class AngleLinear(nn.Module):
         self.in_features = in_features
         self.out_features = out_features
         self.weight = Parameter(torch.Tensor(in_features, out_features))
-        self.weight.data.uniform_(-1, 1).renorm_(2, 1, 1e-5).mul_(1e5)
+        nn.init.kaiming_uniform_(self.weight)
         self.phiflag = phiflag
         self.m = m
         self.mlambda = [
@@ -255,50 +160,22 @@ class AngleLinear(nn.Module):
         ]
 
     def forward(self, input):
-        x = input  # size=(B,F)    F is feature len
-        w = self.weight  # size=(F,Classnum) F=in_features Classnum=out_features
+        x = input
+        w = self.weight.renorm(2, 1, 1e-5).mul(1e5)
+        xlen = x.pow(2).sum(1).pow(0.5)
+        wlen = w.pow(2).sum(0).pow(0.5)
 
-        ww = w.renorm(2, 1, 1e-5).mul(1e5)
-        xlen = x.pow(2).sum(1).pow(0.5)  # size=B
-        wlen = ww.pow(2).sum(0).pow(0.5)  # size=Classnum
-
-        cos_theta = x.mm(ww)  # size=(B,Classnum)
-        cos_theta = cos_theta / xlen.view(-1, 1) / wlen.view(1, -1)
+        cos_theta = x.mm(w) / xlen.view(-1, 1) / wlen.view(1, -1)
         cos_theta = cos_theta.clamp(-1, 1)
 
         if self.phiflag:
             cos_m_theta = self.mlambda[self.m](cos_theta)
-            theta = Variable(cos_theta.data.acos())
+            theta = torch.acos(cos_theta)
             k = (self.m * theta / 3.14159265).floor()
-            n_one = k * 0.0 - 1
-            phi_theta = (n_one ** k) * cos_m_theta - 2 * k
+            phi_theta = (-1) ** k * cos_m_theta - 2 * k
         else:
-            theta = cos_theta.acos()
-            phi_theta = myphi(theta, self.m)
-            phi_theta = phi_theta.clamp(-1 * self.m, 1)
+            phi_theta = cos_theta
 
-        cos_theta = cos_theta * xlen.view(-1, 1)
-        phi_theta = phi_theta * xlen.view(-1, 1)
-        output = (cos_theta, phi_theta)
-        return output  # size=(B,Classnum,2)
-
-    def extract(self, x):
-        # x = x[:, None, ...]
-        x = self.conv1(x)
-        # print('conv1: ', x.size())
-        x = self.bn1(x)
-        x = self.relu(x)
-
-        x = self.layer1(x)
-        # print('layer1: ', x.size())
-        x = self.layer2(x)
-        # print('layer2: ', x.size())
-        x = self.layer3(x)
-        # print('layer3: ', x.size())
-        x = self.layer4(x)
-        # print('layer4: ', x.size())
-
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        # print('flatten: ', x.size())
-        return x
+        cos_theta *= xlen.view(-1, 1)
+        phi_theta *= xlen.view(-1, 1)
+        return cos_theta, phi_theta
